438624ff165691789aab554c7faaea0ff646f8b4
Hoang Ngoc Hung <hung.hoang@ntq-solution.com.vn>
2017-12-13 17:47:05 +0700

    Product Page main: design #1

---
+++ Tapiver/Tapiver/Classes/Modules/ProductPage/Main/TAPProductMainPageViewController.swift
@@ -0,0 +1,20 @@
+//
+//  TAPProductMainPageViewController.swift
+//  Tapiver
+//
+//  Created by HungHN on 12/13/17.
+//  Copyright © 2017 hunghoang. All rights reserved.
+//
+
+import UIKit
+
+class TAPProductMainPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
