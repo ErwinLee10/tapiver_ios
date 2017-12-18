4c26b375555db1ce82ae6903893d3341d2b2093f
Hung vu <Hungvt@Admins-MacBook-Pro.local>
2017-12-18 23:20:39 +0700

    Check out _ part 1

---
+++ Tapiver/Tapiver/Classes/Modules/CheckOut/AddressMethod/TAPAddressMethodController.swift
@@ -0,0 +1,34 @@
+//
+//  TAPAddressMethodController.swift
+//  Tapiver
+//
+//  Created by Hung vu on 12/18/17.
+//  Copyright © 2017 hunghoang. All rights reserved.
+//
+
+import UIKit
+
+class TAPAddressMethodController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        initIb()
+        initData()
+        
+    }
+    func initData() {
+        
+    }
+    func initIb() {
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+   
+
+}
