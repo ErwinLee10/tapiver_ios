1396026a49657d03460e0af1c64d9c1e5931bb31
hahalalamummy <quanglinh95@gmail.com>
2017-12-20 20:22:44 +0700

    Linh: Product page

--- Tapiver/Tapiver/Classes/Modules/ProductPage/Main/TAPProductMainPageViewController.swift
+++ Tapiver/Tapiver/Classes/Modules/ProductPage/Main/TAPProductMainPageViewController.swift
@@ -10,11 +10,35 @@ import UIKit
 
 class TAPProductMainPageViewController: UIViewController {
 
+    @IBOutlet weak var imageContainView: UIView!
+    @IBOutlet weak var likeNumberLabel: UILabel!
+    @IBOutlet weak var likeImage: UIImageView!
+    @IBOutlet weak var colorTableView: UITableView!
+    
+    @IBOutlet weak var titleStackContainView: UIView!
+    @IBOutlet weak var titleStack: UIStackView!
+    
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    @IBOutlet weak var shippingStackContainView: UIView!
+    @IBOutlet weak var shippingStack: UIStackView!
+    
+    @IBOutlet weak var companyImageView: UIImageView!
+    @IBOutlet weak var companyNameLabel: UILabel!
+    @IBOutlet weak var companyLocationLabel: UILabel!
+    @IBOutlet weak var companyFollowerLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func likeProductButtonTap(_ sender: UIButton) {
+    }
+    
+    @IBAction func followCompanyButtonTap(_ sender: UIButton) {
+    }
+    
 }
