fade98ef941b1f1e56164949cd32e25eeaba2a9d
Hung vu <Hungvt@Admins-MacBook-Pro.local>
2017-12-20 23:14:18 +0700

    part 3

--- Tapiver/Tapiver/Classes/Models/CheckOutModel/TAPChecOutEntity.swift
+++ Tapiver/Tapiver/Classes/Models/CheckOutModel/TAPChecOutEntity.swift
@@ -19,3 +19,16 @@ class TAPChecOutEntity: TAPBaseEntity {
     var typeCheckOutCell: CheckOutEntytiType! = .headerShippingAddress
     var addObj: TAPAddressModel?
 }
+
+class TAPListChecOutEntity: TAPBaseEntity {
+    var listCheckOut: [TAPChecOutEntity] = []
+    override func parserResponseArray(dics: [NSDictionary]) {
+        super.parserResponseArray(dics: dics)
+        for dict in dics {
+            let item =  TAPChecOutEntity()
+            item.parserResponse(dic: dict)
+            listCheckOut.append(item)
+        }
+    }
+    
+}
