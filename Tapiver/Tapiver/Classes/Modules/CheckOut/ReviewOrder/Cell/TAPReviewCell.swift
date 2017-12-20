d8141f7d04ed1925a22362347b10866296da29ab
Hung vu <Hungvt@Admins-MacBook-Pro.local>
2017-12-20 08:38:21 +0700

    review order_p1

---
+++ Tapiver/Tapiver/Classes/Modules/CheckOut/ReviewOrder/Cell/TAPReviewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TAPReviewCell.swift
+//  Tapiver
+//
+//  Created by Hung vu on 12/20/17.
+//  Copyright © 2017 hunghoang. All rights reserved.
+//
+
+import UIKit
+
+class TAPReviewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
