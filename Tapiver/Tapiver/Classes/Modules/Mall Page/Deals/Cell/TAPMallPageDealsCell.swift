8e348360901177e69078a3913fb815e93f1d1ad1
canhle <duccanh.qhi@gmail.com>
2017-12-24 13:58:35 +0700

    Update

--- Tapiver/Tapiver/Classes/Modules/Mall Page/Deals/Cell/TAPMallPageDealsCell.swift
+++ Tapiver/Tapiver/Classes/Modules/Mall Page/Deals/Cell/TAPMallPageDealsCell.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import SDWebImage
+import SVProgressHUD
 
 class TAPMallPageDealsCell: UICollectionViewCell {
     @IBOutlet weak var containerView: UIView!
@@ -19,6 +20,8 @@ class TAPMallPageDealsCell: UICollectionViewCell {
     @IBOutlet weak var likeButton: UIButton!
     @IBOutlet weak var numOfLikesLabel: UILabel!
     
+    var productModel: TAPProductModel?
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -26,6 +29,7 @@ class TAPMallPageDealsCell: UICollectionViewCell {
     }
     
     func fillData(product: TAPProductModel) {
+        productModel = product
         let imageURL = URL.init(string: product.sellerPicture ?? "")
         productImgView.sd_setImage(with: imageURL, placeholderImage: nil, options: SDWebImageOptions.retryFailed, completed: nil)
         typeLabel.text = product.brand?.uppercased()
@@ -44,12 +48,62 @@ class TAPMallPageDealsCell: UICollectionViewCell {
         let stdSalePrice = NSNumber(value: variations?.salePrice ?? 0).moneyString()
         realPriceLabel.text = stdSalePrice
         numOfLikesLabel.text = "\(product.likes)"
+        
+        likeButton.isSelected = product.isLikedByThisUser
     }
 
     @IBAction func likeBtnTouched(_ sender: Any) {
+        if TAPGlobal.shared.hasLogin() {
+            if likeButton.isSelected {
+                removeLikeProduct()
+            } else {
+                addLikeProduct()
+            }
+        } else {
+            TAPMainFrame.showLoginPageMain()
+        }
     }
     
     private func setupView() {
         containerView.layer.masksToBounds = true
+    }
+    
+    private func addLikeProduct() {
+        let apiPath = "/api/v1/u/\(TAPGlobal.shared.getLoginModel()?.userId ?? "")/like\(productModel?.id ?? "")"
+        SVProgressHUD.show()
+        TAPWebservice.shareInstance.sendPUTRequest(path: apiPath, parameters: [:]) { [weak self] (isSuccess) in
+            SVProgressHUD.dismiss()
+            if isSuccess {
+                self?.updateLikes(isAdd: true)
+            }
+        }
+    }
+    
+    private func removeLikeProduct() {
+        let apiPath = "/api/v1/u/\(TAPGlobal.shared.getLoginModel()?.userId ?? "")/like/\(productModel?.id ?? "")"
+        SVProgressHUD.show()
+        TAPWebservice.shareInstance.sendDELETERequest(path: apiPath) { [weak self] (isSuccess, responseData) in
+            SVProgressHUD.dismiss()
+            
+            if isSuccess {
+                self?.updateLikes(isAdd: false)
+            }
+        }
+    }
+    
+    private func updateLikes(isAdd: Bool) {
+        if isAdd {
+            productModel?.isLikedByThisUser = true
+            productModel?.likes = (productModel?.likes ?? 0) + 1
+            likeButton.isSelected = true
+            numOfLikesLabel.text = "\(productModel?.likes ?? 0)"
+        } else {
+            productModel?.isLikedByThisUser = false
+            if let likes = productModel?.likes, likes > 0 {
+                productModel?.likes = likes - 1
+            }
+            likeButton.isSelected = true
+            numOfLikesLabel.text = "\(productModel?.likes ?? 0)"
+        }
     }
 }
