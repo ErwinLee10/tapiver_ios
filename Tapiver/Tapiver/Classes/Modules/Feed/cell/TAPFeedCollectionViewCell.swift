a664a5ecf5432f31409f6abbe499b362a8a883dd
Canh Le <canh.le@icehousecorp.com>
2018-01-03 09:35:42 +0700

    fix bug feed tableviewcell

--- Tapiver/Tapiver/Classes/Modules/Feed/cell/TAPFeedCollectionViewCell.swift
+++ Tapiver/Tapiver/Classes/Modules/Feed/cell/TAPFeedCollectionViewCell.swift
@@ -15,9 +15,15 @@ protocol TAPFeedCollectionViewCellDelegate {
 
 class TAPFeedCollectionViewCell: UICollectionViewCell {
     
-    @IBOutlet private weak var pageView: iCarousel!
-    @IBOutlet private weak var pageControl: UIPageControl!
-    @IBOutlet private weak var ivHotNew: UIImageView!
+    //@IBOutlet private weak var pageView: iCarousel!
+    //@IBOutlet private weak var pageControl: UIPageControl!
+	@IBOutlet weak var productImage: UIImageView!
+	@IBOutlet weak var collectionContainerView: UIView!
+	@IBOutlet weak var colorCollectionView: UICollectionView!
+	@IBOutlet weak var colorCollectionViewWidth: NSLayoutConstraint!
+	var colorData: [String]?
+	
+	@IBOutlet private weak var ivHotNew: UIImageView!
     @IBOutlet private weak var persentSaleLbl: UILabel!
     @IBOutlet private weak var ivFree: UIImageView!
     @IBOutlet private weak var likeBtn: UIButton!
@@ -44,11 +50,11 @@ class TAPFeedCollectionViewCell: UICollectionViewCell {
     }
     
     func setupPageView() {
-        self.pageView.type = .linear
-        self.pageView.isPagingEnabled = true
-        self.pageView.delegate = self
-        self.pageView.dataSource = self
-        self.pageControl.numberOfPages = items.count
+//        self.pageView.type = .linear
+//        self.pageView.isPagingEnabled = true
+//        self.pageView.delegate = self
+//        self.pageView.dataSource = self
+//        self.pageControl.numberOfPages = items.count
     }
     
     func hiddenViewAll(isHidden: Bool) {
@@ -66,9 +72,26 @@ class TAPFeedCollectionViewCell: UICollectionViewCell {
         numberLikeLbl.text = String.init(model.likes)
         nameProdctLbl.text = model.brand
         contentLbl.text = model.name
-        items = model.variationsOverview?.pictures ?? []
-        self.pageControl.numberOfPages = items.count
-        pageView.reloadData()
+//        items = model.variationsOverview?.pictures ?? []
+		
+		if checkIfHasManyColor() == false {
+			collectionContainerView.isHidden = true
+		}
+		else {
+			colorCollectionView.delegate = self
+			colorCollectionView.dataSource = self
+//			colorTableView.frame = CGRect(x: colorTableView.frame.origin.x, y: colorTableView.frame.origin.y, width: colorTableView.frame.size.width, height: colorTableView.contentSize.height)
+			if listColor.count <= 4 {
+				colorTableViewHeight.constant = CGFloat(listColor.count * 38)
+			}
+			else {
+				colorTableViewHeight.constant = CGFloat(4 * 38)
+			}
+		}
+//        self.pageControl.numberOfPages = items.count
+//        pageView.reloadData()
+		
+		
         let price = model.variationsOverview?.originalPrice ?? 0
         guard let salePrice = model.variationsOverview?.salePrice else {
             persentSaleLbl.isHidden = true
@@ -86,58 +109,103 @@ class TAPFeedCollectionViewCell: UICollectionViewCell {
         let attStringSaySomething = NSAttributedString(string: "S$" + String.init(price), attributes: attributes)
         priceLbl.attributedText = attStringSaySomething
     }
+	
+	func checkIfHasManyColor(model: TAPProductModel) -> Bool {
+		model.variationsOverview
+		for item in (data?.variations?.listVariations)! {
+			var check = true
+			for color in listColor {
+				if color == item.colorHexCode {
+					check = false
+					break
+				}
+			}
+			if check {
+				listColor.append(item.colorHexCode!)
+			}
+			
+		}
+		if listColor.count > 1 {
+			return true
+		}
+		else {
+			return false
+		}
+	}
 }
 
-extension TAPFeedCollectionViewCell: iCarouselDataSource, iCarouselDelegate {
-    
-    func numberOfItems(in carousel: iCarousel) -> Int {
-        return items.count
-    }
-    
-    func carousel(_ carousel: iCarousel, viewForItemAt index: Int, reusing view: UIView?) -> UIView {
-        var itemView: UIImageView
-        //reuse view if available, otherwise create a new view
-        if let view = view as? UIImageView {
-            itemView = view
-        } else {
-            //don't do anything specific to the index within
-            //this `if ... else` statement because the view will be
-            //recycled and used with other index values later
-            let frame = carousel.frame
-            itemView = UIImageView(frame: CGRect(x: 0, y: 0, width: frame.size.width, height: frame.size.height))
-            itemView.contentMode = .scaleAspectFit
-            itemView.layer.masksToBounds = true
-        }
-        
-        itemView.sd_setImage(with: URL.init(string: items[index]), completed: nil)
-        return itemView
-    }
-    
-    func carouselCurrentItemIndexDidChange(_ carousel: iCarousel) {
-         pageControl.currentPage = carousel.currentItemIndex
-    }
-    
-    func carousel(_ carousel: iCarousel, valueFor option: iCarouselOption, withDefault value: CGFloat) -> CGFloat {
-        if (option == .wrap) {
-            return value * 1.05
-            
-        } else if (option == .spacing) {
-            return 1.0
-        } else if (option == .fadeMax) {
-            if (carousel.type == .custom) {
-                //set opacity based on distance from camera
-                return 0.0
-            }
-            return value;
-        }
-        return value
-    }
-    
-    func carousel(_ carousel: iCarousel, didSelectItemAt index: Int) {
-        delegate?.selectCell(indexPath: indexPath!)
-    }
-    
+extension TAPFeedCollectionViewCell: UICollectionViewDelegate {
+	
 }
+
+extension TAPFeedCollectionViewCell: UICollectionViewDataSource {
+	func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+		if colorData != nil {
+			return (colorData?.count)!
+		}
+		return 0
+	}
+	
+	func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+		let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "TAPFeedColorCollectionViewCell", for: indexPath) as! TAPFeedColorCollectionViewCell
+		cell.setData(hexColor: colorData![indexPath.row])
+		cell.backgroundColor = .clear
+		return cell
+	}
+	
+	
+}
+
+//extension TAPFeedCollectionViewCell: iCarouselDataSource, iCarouselDelegate {
+//
+//    func numberOfItems(in carousel: iCarousel) -> Int {
+//        return items.count
+//    }
+//
+//    func carousel(_ carousel: iCarousel, viewForItemAt index: Int, reusing view: UIView?) -> UIView {
+//        var itemView: UIImageView
+//        //reuse view if available, otherwise create a new view
+//        if let view = view as? UIImageView {
+//            itemView = view
+//        } else {
+//            //don't do anything specific to the index within
+//            //this `if ... else` statement because the view will be
+//            //recycled and used with other index values later
+//            let frame = carousel.frame
+//            itemView = UIImageView(frame: CGRect(x: 0, y: 0, width: frame.size.width, height: frame.size.height))
+//            itemView.contentMode = .scaleAspectFit
+//            itemView.layer.masksToBounds = true
+//        }
+//
+//        itemView.sd_setImage(with: URL.init(string: items[index]), completed: nil)
+//        return itemView
+//    }
+//
+//    func carouselCurrentItemIndexDidChange(_ carousel: iCarousel) {
+//         pageControl.currentPage = carousel.currentItemIndex
+//    }
+//
+//    func carousel(_ carousel: iCarousel, valueFor option: iCarouselOption, withDefault value: CGFloat) -> CGFloat {
+//        if (option == .wrap) {
+//            return value * 1.05
+//
+//        } else if (option == .spacing) {
+//            return 1.0
+//        } else if (option == .fadeMax) {
+//            if (carousel.type == .custom) {
+//                //set opacity based on distance from camera
+//                return 0.0
+//            }
+//            return value;
+//        }
+//        return value
+//    }
+//
+//    func carousel(_ carousel: iCarousel, didSelectItemAt index: Int) {
+//        delegate?.selectCell(indexPath: indexPath!)
+//    }
+//
+//}
 
 
 
